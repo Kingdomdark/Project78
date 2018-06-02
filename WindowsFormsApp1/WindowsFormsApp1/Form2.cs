084fe9bebab6284a2bc5dbac01f5dab8a7c28712
Binh Nguyen <labinhan@hotmail.com>
2018-06-02 16:31:58 +0200

    Quick Test
    Quick Test Executing Python Scripts Through Windows Forms

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string python = @"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\python.exe";
+            string testerino = @"C:\Testerino\testerino.py";
+
+            ProcessStartInfo myProcessStartInfo = new ProcessStartInfo(python);
+
+            myProcessStartInfo.UseShellExecute = false;
+            myProcessStartInfo.RedirectStandardOutput = true;
+
+            myProcessStartInfo.Arguments = testerino;
+            Process myProcess = new Process();
+            myProcess.StartInfo = myProcessStartInfo;
+            Console.WriteLine("Calling Python script");
+
+            myProcess.Start();
+
+
+            StreamReader myStreamReader = myProcess.StandardOutput;
+            string myString = myStreamReader.ReadLine();
+
+            myProcess.WaitForExit();
+            myProcess.Close();
+            Console.WriteLine("Value received from script: " + myString);
+        }
+    }
+}
